8e4364d2edef3f6cf81c6dfd1294ce1bcf29589d
Ismail <ghvh8176@gmail.com>
2025-07-23 22:32:02 -0500

    final

--- 04-express-ejs-app/app.js
+++ 04-express-ejs-app/app.js
@@ -1,11 +1,19 @@
-const express = require('express');
-const path = require('node:path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.set('views', path.join(__dirname, 'views')); // tell Express where views are
-app.set('view engine', 'ejs'); // set EJS as engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const links = [{ href: "/", text: "Home" }, { href: "/about", text: "About" }];
+const users = ["Rose", "Cake", "Biff"];
+
 app.get("/", (req, res) => {
-  res.render("index", { message: "EJS rocks!" });
+  res.render("index", { message: "Welcome to EJS!", links, users });
 });
 
-
+app.listen(3000, () => {
+  console.log("Server is running at http://localhost:3000");
+});
