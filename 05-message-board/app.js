35d5e4713af586e5c3fa429d340f0f71464409d8
Ismail <ghvh8176@gmail.com>
2025-08-04 03:40:15 -0500

    final step

--- 05-message-board/app.js
+++ 05-message-board/app.js
@@ -7,6 +7,9 @@ const app = express()
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
+app.use(express.urlencoded({ extended: true }));
+
+
 const messages = [
   {
     text: "Hi there!",
@@ -24,6 +27,35 @@ app.get("/", (req, res) => {
 res.render("index", {title: "Mini Messageboard", messages: messages})
 
 })
+
+app.get("/new", (req, res) => {
+  res.render("form");
+});
+
+app.post("/new", (req, res) => {
+  const { messageText, messageUser } = req.body;
+
+  messages.push({
+    text: messageText,
+    user: messageUser,
+    added: new Date()
+  });
+
+  res.redirect("/");
+});
+
+app.get("/message/:id", (req, res) => {
+  const id = req.params.id;
+  const message = messages[id];
+
+  if (!message) {
+    return res.status(404).send("Message not found");
+  }
+
+  res.render("message", { title: "Message Detail", message });
+});
+
+
 
 app.listen(3000)
 
