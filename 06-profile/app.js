1d131125b724af5581f73e3e6dd76a056abc8b46
Ismail <ghvh8176@gmail.com>
2025-08-20 13:27:22 -0500

    set up the main page

---
+++ 06-profile/app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const usersRouter = require("./routes/usersRouter")
+
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+app.use("/", usersRouter)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+    if (error) {
+        throw(error)
+    }
+    console.log(`Express app listening on port ${PORT}`)
+})
