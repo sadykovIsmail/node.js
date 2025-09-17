293e9679fd24e6cae3cb5e89d5c849b15357ec4d
Ismail <ghvh8176@gmail.com>
2025-09-17 00:11:48 -0500

    set up data

---
+++ 08-inventory-app/db/pool.js
@@ -0,0 +1,11 @@
+const {Pool} = require('pg')
+
+module.exports = new Pool({
+    host: "localhost",
+    user: "postgres",
+    database: "inventory-app",
+    password: "Ismail2006.,",
+    port: 5432
+
+
+})
