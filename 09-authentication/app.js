2fd611e1e791e348f5e2e380448e7d9a76492270
Ismail <ghvh8176@gmail.com>
2025-09-27 01:04:09 -0500

    serialize

--- 09-authentication/app.js
+++ 09-authentication/app.js
@@ -59,6 +59,22 @@ passport.use(
   })
 );
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
+
+
 
 
 
