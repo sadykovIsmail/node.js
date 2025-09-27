d15bdfe98ad3ac1e89aaf9e3d3906c3f1148a349
Ismail <ghvh8176@gmail.com>
2025-09-27 01:00:30 -0500

    set up local strategy

--- 09-authentication/app.js
+++ 09-authentication/app.js
@@ -40,6 +40,24 @@ app.post("/sign-up", async (req, res, next) => {
   }
 });
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
 
 
 
