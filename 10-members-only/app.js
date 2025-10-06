7c48a90e127ca3afa9f56960cbb00bc1bd5d737a
Ismail <ghvh8176@gmail.com>
2025-10-06 17:17:50 -0500

    create

---
+++ 10-members-only/app.js
@@ -0,0 +1,7 @@
+const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
