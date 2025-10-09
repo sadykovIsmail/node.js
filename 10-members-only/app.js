55394055cfdaff87dfc3dea86a30d040b72c1d63
Ismail <ghvh8176@gmail.com>
2025-10-09 00:03:31 -0500

    structure

--- 10-members-only/app.js
+++ 10-members-only/app.js
@@ -5,3 +5,12 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
+
+const pool = new Pool({
+  host: "localhost",
+  user: "postgres",        // your Postgres username
+  database: "top_users",   // your DB name
+  password: "Ismail2006.,",// your Postgres password
+  port: 5432,
+});
+
