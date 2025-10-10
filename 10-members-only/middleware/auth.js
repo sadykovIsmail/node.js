9cd6943676ad219bc553f811dd2467eb5d48fbbb
Ismail <ghvh8176@gmail.com>
2025-10-10 00:55:14 -0500

    check if user member

--- 10-members-only/middleware/auth.js
+++ 10-members-only/middleware/auth.js
@@ -1,8 +1,13 @@
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
+// Check if user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
   res.redirect("/log-in");
 }
 
-module.exports = { checkAuthenticated };
+// Check if user is a member
+function isMember(req, res, next) {
+  if (req.isAuthenticated() && req.user.membership_status) return next();
+  res.status(403).send("You are not a member. Access denied.");
+}
+
+module.exports = { isLoggedIn, isMember };
