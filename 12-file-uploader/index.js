1a54d3e5552d272f5d1195d507fe128e854ddaef
Ismail <ghvh8176@gmail.com>
2025-10-20 22:01:49 -0500

    sign up page

---
+++ 12-file-uploader/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("path");
+const { PrismaClient } = require("@prisma/client");
+const bodyParser = require("body-parser");
+
+const prisma = new PrismaClient();
+const app = express();
+
+// setup ejs
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// home
+app.get("/", (req, res) => {
+  res.send("Server is working!");
+});
+
+// show signup form
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+// handle signup
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // create user in database
+    const newUser = await prisma.user.create({
+      data: { email, password },
+    });
+
+    res.send(`User ${newUser.email} created successfully!`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error creating user.");
+  }
+});
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
