b52681c45cc0447a0f8e058bc8ebe91fce3f1d42
Ismail <ghvh8176@gmail.com>
2025-10-25 23:01:10 -0500

    make run on all devices

--- 10-members-only/app.js
+++ 10-members-only/app.js
@@ -252,4 +252,6 @@ app.use("/", authRoutes);
 app.use("/members", memberRoutes);
 
 // Start server
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on http://0.0.0.0:3000');
+});
